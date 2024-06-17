bad9a1528f4ae2b793e182b5653580b7b4c36cf0
Hugo-walando <hugowalando@gmail.com>
2024-06-17 14:56:39 +0200

    feat: Create Pnj component

---
+++ src/app/components/pnj/pnj.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PnjComponent } from './pnj.component';
+
+describe('PnjComponent', () => {
+  let component: PnjComponent;
+  let fixture: ComponentFixture<PnjComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PnjComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PnjComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/pnj/pnj.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pnj',
+  standalone: true,
+  imports: [],
+  templateUrl: './pnj.component.html',
+  styleUrl: './pnj.component.scss'
+})
+export class PnjComponent {
+
+}
