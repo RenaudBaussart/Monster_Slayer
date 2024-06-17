4dcba50d076940d16866ca9e6cef98113226053e
Hugo-walando <hugowalando@gmail.com>
2024-06-17 14:55:52 +0200

    feat: import pnj component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HealthBarComponent } from './components/health-bar/health-bar.component';
 import { PlayerComponent } from './components/player/player.component';
+import { PnjComponent } from './components/pnj/pnj.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HealthBarComponent, PlayerComponent],
+  imports: [RouterOutlet, HealthBarComponent, PlayerComponent, PnjComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
