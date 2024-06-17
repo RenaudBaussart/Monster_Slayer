6a7441ab1c0a9894145328c60d4b91516ffe8b58
renaudbaussart <renaud.baussart@proton.me>
2024-06-17 10:41:02 +0200

    feat: generate health bar comp

---
+++ src/app/components/health-bar/health-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-health-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './health-bar.component.html',
+  styleUrl: './health-bar.component.scss'
+})
+export class HealthBarComponent {
+
+}
