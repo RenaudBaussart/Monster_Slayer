987a8bfa48a40d8b45c3f75a58e934997226ffe0
MrLkuch <kucheida.lucas@gmail.com>
2024-06-19 10:25:08 +0200

    feat(player/): move player component inside module

---
+++ src/app/player-module/player/player.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { PlayerHealthBarComponent } from '../player-health-bar/player-health-bar.component';
+
+@Component({
+  selector: 'app-player',
+  templateUrl: './player.component.html',
+  styleUrl: './player.component.scss'
+})
+export class PlayerComponent {
+
+}
