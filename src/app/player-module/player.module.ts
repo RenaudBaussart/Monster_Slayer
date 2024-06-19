5184eefd53c358bb4864316c31733cab5027863d
MrLkuch <kucheida.lucas@gmail.com>
2024-06-19 10:26:34 +0200

    feat(player.module.ts): finish imports & exports

--- src/app/player-module/player.module.ts
+++ src/app/player-module/player.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayerButtonComponent } from './player-button/player-button.component';
 import { PlayerHealthBarComponent } from './player-health-bar/player-health-bar.component';
+import { PlayerComponent } from './player/player.component';
 import { CombatCoreService } from '../services/combat-core.service';
 import { DeathService } from '../services/death.service';
 
 @NgModule({
   providers: [CombatCoreService, DeathService],
   declarations: [
+    PlayerComponent,
     PlayerButtonComponent,
-    PlayerHealthBarComponent
+    PlayerHealthBarComponent,
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [PlayerButtonComponent, PlayerComponent, PlayerHealthBarComponent]
 })
-export class PlayerModuleModule { }
+export class PlayerModule { }
