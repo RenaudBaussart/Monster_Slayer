9d95e81d91f67063ba1aa3e8e14bd0cfb1920c78
MrLkuch <kucheida.lucas@gmail.com>
2024-06-20 12:05:50 +0200

    refactor(pnj-health-bar/): move and modify component to use it in new module

--- src/app/pnj-module/pnj-health-bar/pnj-health-bar.component.spec.ts
+++ src/app/pnj-module/pnj-health-bar/pnj-health-bar.component.spec.ts
@@ -8,7 +8,7 @@ describe('PnjHealthBarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PnjHealthBarComponent]
+      declarations: [PnjHealthBarComponent]
     })
     .compileComponents();
 
